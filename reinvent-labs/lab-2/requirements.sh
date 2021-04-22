a8d9e2809a64a1927df9e7bff0c80aaa3d64a186
Dylan Jackson <jacdylan@amazon.com>
2021-04-22 16:50:28 -0700

    Fixing regression in installing dependencies in GAN lab. Locking down dependencies to working versions in GAN lab.

--- reinvent-labs/lab-2/requirements.sh
+++ reinvent-labs/lab-2/requirements.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+
 source activate python3
 conda update --all --y 
 pip install tensorflow-gpu==1.14.0
 pip install numpy==1.16.4
-pip install pretty_midi
-pip install pypianoroll
-pip install music21
-pip install seaborn
-pip install --ignore-installed moviepy
+pip install pretty_midi==0.2.9
+pip install pypianoroll==0.5.3
+pip install music21==6.7.1
+pip install seaborn==0.11.1
+pip install --ignore-installed moviepy==1.0.3
